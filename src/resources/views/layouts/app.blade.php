f7a9cc2fce1772ba659d13f33a00a018406f06cc
Ryunosuke Sekido <sekiryu1998@gmail.com>
2023-12-25 22:05:28 +0900

    ツイート関連のviewを追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -52,6 +52,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item me-5 mt-2">
+                                <a href="{{ url('tweets/create') }}" class="btn btn-md btn-primary">ツイートする</a>
+                            </li>
                             <li class="nav-item">
                                 <img src="{{ asset('storage/profile_image/' .auth()->user()->profile_image) }}" class="rounded-circle" width="50" height="50">
                             </li>
