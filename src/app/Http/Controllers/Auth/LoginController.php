4641ba9cb3689554258c6da20e492e23b5909bef
Ryunosuke Sekido <sekiryu1998@gmail.com>
2023-12-28 10:29:46 +0900

    ログイン後とログアウト後のページ遷移先を変更

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,32 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * ログイン後のリダイレクト先を変更
+     *
+     * @return void
+     */
+    public function redirectPath()
+    {
+        return '/tweets';
+    }
+
+    /**
+     * ログアウト後のリダイレクト先を変更
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
